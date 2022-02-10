57b1b85fa3ec6d2d6a33c34a284b7d21e29c3b86
swaroopc236 <swaroopc236@gmail.com>
2022-02-10 21:31:14 +0530

    Websocket configs

--- ws.js
+++ ws.js
@@ -1,6 +1,6 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 5050 });
+const wss = new WebSocket.Server({ port: process.env.WS_PORT });
 
 wss.on('connection', (ws) => {
 	console.log('Client connected');
