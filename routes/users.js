6c7456a43db52b44ce15a4776cc2cebebf1aeb53
Swaroop Chavhan <53576249+swaroopc236@users.noreply.github.com>
2022-02-16 14:30:07 +0530

    rearranged users routes

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,6 @@ const { requireAuth, requireAdmin } = require('../middleware/authMiddleware');
 
 router.get('/', userController.getUsers);
 
-router.get('/:userId', userController.getUserById);
-
 router.post('/signup', userController.signupUser);
 
 router.post('/login', userController.loginUser);
@@ -13,6 +11,8 @@ router.post('/login', userController.loginUser);
 router.put('/:userId', requireAuth, userController.updateUser);
 
 router.get('/logout', userController.logoutUser);
+
+router.get('/:userId', userController.getUserById);
 
 router.delete('/:userId', requireAuth, requireAdmin, userController.deleteUser);
 
